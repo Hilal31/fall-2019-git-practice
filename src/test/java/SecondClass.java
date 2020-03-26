f5c1e06b7de2093136e412495c89934259e68cbb
Hilal31 <g.iclal91@gmail.com>
2020-03-25 21:12:57 -0400

    dfnkwfn

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -2,6 +2,7 @@ public class SecondClass {
     //change 1status
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("corona days-25-03-2020");
     }
 
 }
